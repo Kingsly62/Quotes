344cf3292465a2d4a94945d334b23ed247e0baf0
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-01-04 18:50:34 +0300

    Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { RateUsComponent } from './rate-us/rate-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutUsComponent,
+    ContactComponent,
+    RateUsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+   title='SILICON-TECH COMPANY'
   constructor() { }
 
   ngOnInit(): void {
